14dbb6ff385803c25d0572bc9ae5b92354d94b3f
nwils000 <nwils000@gmail.com>
2023-01-27 17:13:11 -0500

    Added 3 sprites and body component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,13 @@
 import "../CSS/app.css";
 import React from "react";
 import Navbar from "./Navbar";
+import Body from "./Body";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Body />
     </>
   );
 }
