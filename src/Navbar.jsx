c8c4562b17d47077f728e32412205d87b4e7da31
nwils000 <nwils000@gmail.com>
2023-01-27 09:50:03 -0500

    Made Navbar components.

---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarUpper from "./NavbarUpper";
+import NavbarLower from "./NavbarLower";
+
+export default function Navbar() {
+  return (
+    <>
+      <NavbarUpper />
+      <NavbarLower />
+    </>
+  );
+}
---
+++ src/NavbarLower.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NavbarLower() {
+  return <div>NavbarLower</div>;
+}
