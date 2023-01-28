ac30ff58d5e28a6206f6ca71104a7d4f85825fa5
nwils000 <nwils000@gmail.com>
2023-01-27 19:33:18 -0500

    Finished xml for FooterUpper

--- src/Components/FooterUpper.jsx
+++ src/Components/FooterUpper.jsx
@@ -5,7 +5,60 @@ export default function FooterUpper() {
     <div>
       <button>Back to top</button>
       <div>
-        <ul></ul>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div>
+        <a href="/">Logo</a>
+        <button>English</button>
+        <button>
+          {/* <img src="" alt="" /> */}
+          United States
+        </button>
       </div>
     </div>
   );
