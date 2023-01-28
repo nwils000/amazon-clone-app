7c2c803691e347ee4cb6f51554479a3e8ba9f5c4
nwils000 <nwils000@gmail.com>
2023-01-27 19:56:58 -0500

    Added text to footer

--- src/Components/FooterLower.jsx
+++ src/Components/FooterLower.jsx
@@ -5,189 +5,152 @@ export default function FooterLower() {
     <div>
       <div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Music</h3>
+          <p>Stream millions of songs</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Advertising</h3>
+          <p>Find, attract, and engage customers</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Drive</h3>
+          <p>Cloud storage from Amazon</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>6pm</h3>
+          <p>Score deals on fashion brands</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>AbeBooks</h3>
+          <p>Books, art & collectibles</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>ACX</h3>
+          <p>Audiobook Publishing Made Easy</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Sell on Amazon</h3>
+          <p>Start a Selling Account</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Business</h3>
+          <p>Everything For Your Business</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Fresh</h3>
+          <p>Groceries & More Right To Your Door</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>AmazonGlobal</h3>
+          <p>Ship Orders Internationally</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Home Services</h3>
+          <p>Experienced Pros Happiness Guarantee</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Ignite</h3>
+          <p>Sell your original Digital Educational Resources</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Web Services</h3>
+          <p>Scalable Cloud Computing Services</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Audible</h3>
+          <p>Listen to Books & Original Audio Performances</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Book Depository</h3>
+          <p>Books With Free Delivery Worldwide</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Box Office Mojo</h3>
+          <p>Find Movie Box Office Data</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>ComiXology</h3>
+          <p>Thousands of Digital Comics</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>DPReview</h3>
+          <p>Digital Photography</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Fabric</h3>
+          <p>Sewing, Quilting & Knitting</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Goodreads</h3>
+          <p>Book reviews & recommendations</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>IMDb</h3>
+          <p>Movies, TV & Celebrities</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>IMDbPro</h3>
+          <p>Get Info Entertainment Professionals Need</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Kindle Direct Publishing</h3>
+          <p>Indie Digital & Print Publishing Made Easy</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Photos</h3>
+          <p>Unlimited Photo Storage Free With Prime</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Prime Video Direct</h3>
+          <p>Video Distribution Made Easy</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Shopbop</h3>
+          <p>Designer Fashion Brands</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Warehouse</h3>
+          <p>Great Deals on Quality Used Products</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Whole Foods Market</h3>
+          <p>America’s Healthiest Grocery Store</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Woot!</h3>
+          <p>Deals and Shenanigans</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Zappos</h3>
+          <p>Shoes & Clothing</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Ring</h3>
+          <p>Smart Home Security Systems</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>eero WiFi</h3>
+          <p>Stream 4K Video in Every Room</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Blink</h3>
+          <p>Smart Security for Every Home</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Neighbors App</h3>
+          <p>Real-Time Crime & Safety Alerts</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Subscription Boxes</h3>
+          <p>Top subscription boxes – right to your door</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>PillPack</h3>
+          <p>Pharmacy Simplified</p>
         </div>
         <div>
-          <h4>
-            <p></p>
-          </h4>
+          <h3>Amazon Renewed</h3>
+          <p>Like-new products you can trust</p>
         </div>
       </div>
       <div>
--- src/Components/FooterUpper.jsx
+++ src/Components/FooterUpper.jsx
@@ -6,50 +6,54 @@ export default function FooterUpper() {
       <button>Back to top</button>
       <div>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>Get to Know Us</li>
+          <li>Careers</li>
+          <li>Amazon Newsletter</li>
+          <li>About Amazon</li>
+          <li>Accessibility</li>
+          <li>Sustainability</li>
+          <li>Press Center</li>
+          <li>Investor Relations</li>
+          <li>Amazon Devices</li>
+          <li>Amazon Science</li>
         </ul>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>Make Money with Us</li>
+          <li>Sell products on Amazon</li>
+          <li>Sell apps on Amazon</li>
+          <li>Supply to Amazon</li>
+          <li>Protect & Build Your Brand</li>
+          <li>Become an Affiliate</li>
+          <li>Become a Delivery Driver</li>
+          <li>Start a package delivery business</li>
+          <li>Advertise Your Products</li>
+          <li>Self-Publish with Us</li>
+          <li>Host an Amazon Hub</li>
+          <li>See More Ways to Make Money</li>
         </ul>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>Amazon Payment Products</li>
+          <li>Amazon Rewards Visa Signature Cards</li>
+          <li>Amazon Store Card</li>
+          <li>Amazon Secured Card</li>
+          <li>Amazon Business Card</li>
+          <li>Shop with Points</li>
+          <li>Credit Card Marketplace</li>
+          <li>Reload Your Balance</li>
+          <li>Amazon Currency Converter</li>
         </ul>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>Let Us Help You</li>
+          <li>Amazon and COVID-19</li>
+          <li>Your Account</li>
+          <li>Your Orders</li>
+          <li>Shipping Rates & Policies</li>
+          <li>Amazon Prime</li>
+          <li>Returns & Replacements</li>
+          <li>Manage Your Content and Devices</li>
+          <li>Your Recalls and Product Safety Alerts</li>
+          <li>Amazon Assistant</li>
+          <li>Help</li>
         </ul>
       </div>
       <div>
