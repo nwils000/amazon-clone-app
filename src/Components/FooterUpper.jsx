54c8f4bbca65a77ee45d15d1d87c58bbc9150504
nwils000 <nwils000@gmail.com>
2023-01-27 19:29:35 -0500

    Set up components for footer

---
+++ src/Components/FooterUpper.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function FooterUpper() {
+  return (
+    <div>
+      <button>Back to top</button>
+      <div>
+        <ul></ul>
+      </div>
+    </div>
+  );
+}
