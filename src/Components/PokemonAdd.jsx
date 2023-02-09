1f88b40a83f817dd7c96716630f7596aab8ab469
nwils000 <100631201+nwils000@users.noreply.github.com>
2023-02-09 17:13:28 -0500

    Added error message for laptop screens

--- src/Components/PokemonAdd.jsx
+++ src/Components/PokemonAdd.jsx
@@ -3,8 +3,14 @@ import React from "react";
 export default function PokemonAdd() {
   return (
     <div className="pokemon-grid">
+      <div className="not-compatible-large">
+        Your screen size is not compatible.
+        <p>
+          For optimal performance either zoom out, or use a different monitor.
+        </p>
+      </div>
       <div className="pokemonSprite imgbin_pikachu-pokmon-x-and-y-haunter-gengar-png"></div>
-      <h1>Brand New Pokémon Merch</h1>
+      <h1 className="merch">Brand New Pokémon Merch</h1>
       <div className="pokemonSprite imgbin_pokmon-x-and-y-pokmon-go-ash-ketchum-pikachu-squirtle-png"></div>
       <div className="pokemonSprite imgbin_pokmon-x-and-y-torchic-pokkn-tournament-may-png"></div>
     </div>
